a314050e39ef2a0e962aa8149058be3382685496
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    pdx215: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking the fingerprint scanner
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- device.mk
+++ device.mk
@@ -413,6 +413,10 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lahaina-libperfmgr \
