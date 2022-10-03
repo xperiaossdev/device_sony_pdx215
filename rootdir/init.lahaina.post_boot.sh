5b71a18a3dd85840fa040d6a1a99d0000dda7672
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-03 23:19:34 +0530

    pdx215: rootdir: Import cgroup tuning from gs101
    Inspired by
    https://github.com/PixelOS-Devices/device_xiaomi_lisa/commit/936b04daff511c21f5a6ea9b201cfd5ef3013bc8
    
    Change-Id: I01825812ba9898e142ef12ca8bcceef7d7a3e34a
    Signed-off-by: bheatleyyy <sugakesshaa@pixelos.net>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- rootdir/init.lahaina.post_boot.sh
+++ rootdir/init.lahaina.post_boot.sh
@@ -43,3 +43,19 @@ echo 0 > /dev/stune/schedtune.boost
 echo 1 > /dev/stune/top-app/schedtune.prefer_idle
 echo 0 > /dev/stune/top-app/schedtune.prefer_high_cap
 echo 10 > /dev/stune/top-app/schedtune.boost 10
+
+# uclamp tuning
+echo 50 > /dev/cpuctl/background/cpu.uclamp.max
+echo 50 > /dev/cpuctl/system-background/cpu.uclamp.max
+echo 60 > /dev/cpuctl/dex2oat/cpu.uclamp.max
+# Setup cpu.shares to throttle background groups (bg ~ 5% sysbg ~ 5% dex2oat ~2.5%)
+echo 1024 > /dev/cpuctl/background/cpu.shares
+echo 1024 > /dev/cpuctl/system-background/cpu.shares
+echo 512 > /dev/cpuctl/dex2oat/cpu.shares
+echo 20480 > /dev/cpuctl/system/cpu.shares
+# We only have system and background groups holding tasks and the groups below are empty
+echo 20480 > /dev/cpuctl/camera-daemon/cpu.shares
+echo 20480 > /dev/cpuctl/foreground/cpu.shares
+echo 20480 > /dev/cpuctl/nnapi-hal/cpu.shares
+echo 20480 > /dev/cpuctl/rt/cpu.shares
+echo 20480 > /dev/cpuctl/top-app/cpu.shares
