0629c991987bb50984a42e6b9ce0018b6c5af628
pjgowtham <pjgowtham@gmail.com>
2022-12-17 18:39:28 +0530

    sagami: Load adsp_loader_dlkm for battery status in recovery
    Change-Id: Ia9b62249176dc6b86fd7ab40697940cb83c45cc2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,6 +95,8 @@ TARGET_KERNEL_NO_GCC := true
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/configs/modules/modules.blocklist
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/configs/modules/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/configs/modules/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/configs/modules/modules.include.recovery))
 TARGET_MODULE_ALIASES += wlan.ko:qca_cld3_wlan.ko
 
 # Use External DTC
