9ebc0fde25cc1cd3eec36ef2ab7b258b687f9c06
saku-bruh <notwolfyy@gmail.com>
2023-01-03 23:58:12 +0200

    sagami: bring up-to cherish

--- common.mk
+++ common.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-cherish
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
