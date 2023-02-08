1fac62e4e841f37ccee91c6cabc2e217a7ae0125
Luofan Chen <clfbbn@gmail.com>
2023-02-08 13:52:00 +0800

    pdx215: overlay: Use correct LED light capabilities
    * Also define custom overlay folder
    Change-Id: I7559592af4e8265601cc789ff01ecfcc9cfc03a1

--- device.mk
+++ device.mk
@@ -48,7 +48,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay/custom
 
 # Device Overlays
 PRODUCT_PACKAGES += \
