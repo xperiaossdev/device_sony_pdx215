df2d3fefbd0d4d11b6f974391af545b06471ea78
saku-bruh <notwolfyy@gmail.com>
2023-02-24 04:03:06 +0200

    sagami: bring up-to rice
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,7 +177,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/rice/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
