3566a01611432aba368019c1be7507c7075173f6
Wei Wang <wvw@google.com>
2023-03-05 00:50:04 +0600

    pdx215: rootdir: Boot time tuning
    Test: build and boottime test
    Change-Id: Ib8f31d39501b4747f0c268d2dd87e814bcc7843b
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- rootdir/init.lahaina.post_boot.sh
+++ rootdir/init.lahaina.post_boot.sh
@@ -23,3 +23,8 @@ echo 128 > /sys/block/dm-9/queue/read_ahead_kb
 # cpuset
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+
+# Setup runtime schedTune
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
