ace42800f3e5141948df4553cde4990e761192ee
saku-bruh <notwolfyy@gmail.com>
2023-03-19 18:40:38 +0200

    sagami: fix vendor sec patch year
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -236,7 +236,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2022-03-01
+VENDOR_SECURITY_PATCH := 2023-03-01
 
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
