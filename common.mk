ec93151c9c1f3ba6db5a6dba42a428dfc03a58a3
saku-bruh <notwolfyy@gmail.com>
2023-04-03 01:27:24 +0300

    fixup! sagami: drop dolby vision
    - Also re-add Dexpreopt
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- common.mk
+++ common.mk
@@ -357,12 +357,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    libavservices_minijail.vendor \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_hidl@1.1.vendor \
-    libavservices_minijail_vendor \
-    libcodec2_soft_common.vendor \
-    libsfplugin_ccodec_utils.vendor
+    libcodec2_hidl@1.0.vendor
 
 # Media (Device)
 PRODUCT_COPY_FILES += \
