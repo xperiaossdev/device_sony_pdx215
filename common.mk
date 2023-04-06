b61a8fb8c10183a7e3250676510b002f97a17c74
saku-bruh <notwolfyy@gmail.com>
2023-04-06 07:54:08 +0300

    sagami: bring up-to crdroid
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- common.mk
+++ common.mk
@@ -36,7 +36,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-cherish
+    $(LOCAL_PATH)/overlay-lineage
 
 # Common Overlays
 PRODUCT_PACKAGES += \
