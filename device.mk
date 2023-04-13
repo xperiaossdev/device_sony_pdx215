d713c3896b635a178a4890d45325835fe07beca5
saku-bruh <notwolfyy@gmail.com>
2023-04-13 22:23:09 +0300

    pdx215: drop unneeded overlay
    - Adding overlay/custom isn't needed just overlay is enough to build overlay/custom.
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- device.mk
+++ device.mk
@@ -45,8 +45,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay/custom
+    $(LOCAL_PATH)/overlay
 
 # Device Overlays
 PRODUCT_PACKAGES += \
