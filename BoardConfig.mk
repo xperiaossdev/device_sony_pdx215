e8ffb39ff7448439b2b0bc50c0e38d4f21e4777c
saku-bruh <notwolfyy@gmail.com>
2023-04-18 04:54:56 +0300

    pdx215: fix vintf for dolby
    - Drop Smart Pixels
    - Drop non-working blank overlay
    - Fix vintf stuff to make Dolby work
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,12 +192,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config/config.fs
 TARGET_EXCLUDES_AUDIOFX := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE :+= $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
