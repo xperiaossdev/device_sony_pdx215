LineageOS/android_device_sony_pdx215

pdx215: Update blobs to 61.2.A.0.447
Change-Id: I743ebfed25de23999ba91266bbd6faec1fa9edf1

lineage_pdx215.mk
@@ -40,7 +40,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
-    PRIVATE_BUILD_DESC="XQ-BC72-user 13 61.2.A.0.410 061002A000041000046651803 release-keys"
+    PRIVATE_BUILD_DESC="XQ-BC72-user 13 61.2.A.0.447 061002A000044700046651803 release-keys"
 
-BUILD_FINGERPRINT := Sony/XQ-BC72/XQ-BC72:13/61.2.A.0.410/061002A000041000046651803:user/release-keys
-
+BUILD_FINGERPRINT := Sony/XQ-BC72/XQ-BC72:13/61.2.A.0.447/061002A000044700046651803:user/release-keys

xperiaossdev/device_sony_pdx215

lineage_pdx215.mk
@@ -62,6 +62,7 @@ PRODUCT_MODEL := Xperia 1 III
 PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="XQ-BC52-user 13 61.2.A.0.410 061002A000039600046651803 release-keys"
+    PRIVATE_BUILD_DESC="XQ-BC52-user 13 61.2.A.0.447 061002A000044700046651803 release-keys"
 
-BUILD_FINGERPRINT := Sony/qssi/qssi:13/61.2.A.0.410/061002A000038800046651803:user/release-keys
+
+BUILD_FINGERPRINT := Sony/XQ-BC52/XQ-BC52:13/61.2.A.0.447/061002A000044700046651803:user/release-keys